d4dab39838b50841f6fc915cc80c801084acbe10
Xinhao Song <songxinhao@hotmail.com>
2019-07-28 17:40:21 -0500

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,6 +2,7 @@
 #include <string>
 using namespace std;
 
+// Templated functions
 template<typename T>
 T * arrMax(T * array, size_t n){
     if(n==0){
@@ -29,6 +30,50 @@ T * arrMin(T * array, size_t n){
     }
     return ans;
 }
+
+// Templated class
+template<typename T>
+class Array{
+    private:
+        T * data;
+        size_t size;
+    public:
+        // Constructor without parameter
+        Array(): data(NULL), size(0){}
+        // Constructor with parameters
+        explicit Array(size_t n): data(new T[n]()), size(n){}
+        // Copy constructor
+        Array(const Array & rhs): data(NULL), size(rhs.size){
+            (*this) = rhs;
+        }
+        // Destructor
+        ~Array(){
+            delete[] data;
+        }
+        // Overload assignment operator
+        Array & operator=(const Array & rhs){
+            if(this != &rhs){
+                T * temp = new T[rhs.size];
+                for(int i=0; i<rhs.size; i++){
+                    temp[i] = rhs.data[i];
+                }
+                delete[] data;
+                data = temp;
+                size = rhs.size;
+            }
+            return *this;
+        }
+        // Overload index operator
+        T & operator[](unsigned ind){
+            return data[ind];
+        }
+        const T & operator[](unsigned ind) const{
+            return data[ind];
+        }
+        size_t getSize() const{
+            return size;
+        }
+};
 
 int main(void){
     int intArray[] = {1,3,6,9,2,7};
