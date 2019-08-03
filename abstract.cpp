18a266a733ec2208a540109c83458a94bd66cf53
Xinhao Song <songxinhao@hotmail.com>
2019-08-03 14:43:53 -0500

    Update abstract.cpp

--- abstract.cpp
+++ abstract.cpp
@@ -2,55 +2,87 @@
 #include <algorithm>
 #include <string>
 #include <math.h>
+#include <vector>
 using namespace std;
 
-class Point{
-    private:
-        int x;
-        int y;
-    public:
-        //default constructor
-        Point(): x(0), y(0) {}
-        Point(int init_x, int init_y): x(init_x), y(init_x){}
-        void setPoint(int a, int b){
-            x = a;
-            y = b;
-        }
-        int getX(void) const{
-            return x;
-        }
-        int getY(void) const{
-            return y;
-        }
-        void printPoint(void){
-            cout<<"("<<x<<","<<y<<")"<<endl;
-        }
-        Point operator + (const Point& rhs) const{
-            Point ans;
-            ans.x = x + rhs.x;
-            ans.y = y + rhs.y;
-            return ans;
-        } 
-        Point & operator += (const Point & rhs){
-            x += rhs.x;
-            y += rhs.y;
-            return *this;
-        }
-        double getDistance(const Point & obj) const{
-            return sqrt(pow(double(x-obj.x),2) + pow(double(y-obj.y),2));
-        }
-        double getSlope(const Point & obj) const{
-            return (y-obj.y)/(x-obj.x);
-        }
-};
 
 class Shape{
     public:
         // Declaration of an abstract method (aka pure virtual member function)
-        virtual bool containsPoint(const Point & p) const = 0;
+        virtual double getArea() const = 0;
+        // Destructor, also needs to be virtual
+        virtual ~Shape() {}
 };
 
+class Rectangle: public Shape{
+    private:
+        double width;
+        double height;
+    public:
+        // Constructor with parameters
+        Rectangle(double w, double h): width(w), height(h) {}
+        // A concrete implementation of the inherited abstract method
+        virtual double getArea() const{
+            return width * height;
+        }
+};
+
+class Triangle: public Shape{
+    private:
+        double base;
+        double height;
+    public:
+        // Constructor with parameters
+        Triangle(double b, double h): base(b), height(h) {}
+        // Another concrete implementation of the inherited abstract method
+        virtual double getArea() const{
+            return 0.5 * base * height;
+        }
+};
+
+// Use a macro to define the invariable pi
+#define PI 3.1415926535
+class Circle: public Shape{
+    private:
+        double radius;
+    public:
+        // Constructor with parameter
+        // Note this constructor should be declared explicit
+        // since it only takes one parameter
+        explicit Circle(double r): radius(r) {}
+        // Yet another concrete implementation of the inherited abstract method
+        virtual double getArea() const{
+            return PI * radius * radius;
+        }
+};
+
+double sumAreas(const vector<Shape *> & shapes){
+    // Use const_iterator instead of iterator to
+    // avoid accidentally modifying data
+    vector<Shape *>::const_iterator it = shapes.begin();
+    double answer = 0;
+    while(it != shapes.end()){
+        // Subtype polymorphism can only be utilized
+        // via pointers or references
+        Shape * ptr = *it;
+        // getArea() will be dynamically dispatched
+        // based on the dynamic type
+        answer += ptr->getArea();
+        ++it;
+    }
+    return answer;
+}
 
 int main(){
+    vector<Shape *> v;
+    v.push_back(new Rectangle(2, 2));
+    v.push_back(new Circle(1));
+    v.push_back(new Triangle(1,2));
+    cout<<sumAreas(v)<<endl;
+    vector<Shape *>::iterator it = v.begin();
+    while(it != v.end()){
+        delete *it;
+        ++it;
+    }
     return EXIT_SUCCESS;
 }
