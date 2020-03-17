1125bee6673bac23911de1c63daac124dd6beb9d
Xinhao Song <songxinhao@hotmail.com>
2020-03-16 23:33:38 -0500

    Create Graph.cpp

---
+++ Graph.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+class Graph{
+public:
+    unsigned addNode();
+    void removeNode(unsigned whichNode);
+    void addEdge(unsigned from, unsigned to, double weight);
+    void removeEdge(undigned from, unsigned to);
+    unsigned getNodeCount() const;
+    set<pair<unsigned, double> > getAdjacencies(unsigned whichNode) const;
+    double getEdge(unsigned from, unsigned to) const;
+    bool isAdjacent(unsigned from, unsigned to) const;
+};
+
+int main()
+{
+    return EXIT_SUCCESS;
+}
