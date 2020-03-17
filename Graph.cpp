896b14bbc736a9f3480f28ac3a15eafd718d9d4e
Xinhao Song <songxinhao@hotmail.com>
2020-03-16 23:46:54 -0500

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -8,7 +8,7 @@ public:
     unsigned addNode();
     void removeNode(unsigned whichNode);
     void addEdge(unsigned from, unsigned to, double weight);
-    void removeEdge(undigned from, unsigned to);
+    void removeEdge(unsigned from, unsigned to);
     unsigned getNodeCount() const;
     set<pair<unsigned, double> > getAdjacencies(unsigned whichNode) const;
     double getEdge(unsigned from, unsigned to) const;
