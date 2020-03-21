ab55debc8547456d4b1285f00718aaa0ab62414e
Xinhao Song <songxinhao@hotmail.com>
2020-03-21 17:43:53 -0500

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -11,6 +12,10 @@ private:
 public:
     Vertix(char name){
         id = name;
+    }
+    Vertix(char name, map<char, int> edges){
+        id = name;
+        info = edges;
     }
     void addEdge(char id, int weight){
         info.insert(pair<char, int>(id, weight));
@@ -23,33 +28,46 @@ public:
     }
 };
 
+// Overload operator == for class Vertix
+bool operator==(Vertix lhs, Vertix rhs){
+    if(lhs.getId() == rhs.getId()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 class Path{
 private:
-    vector<char> route;
+    vector<Vertix> route;
 public:
     unsigned int getSize(){
         return route.size();
     }
     void addStop(Vertix node){
-        route.push_back(node.getId());
+        route.push_back(node);
+    }
+    Vertix getLastNode(){
+        return route.back();
     }
     void printPath(){
         if(route.size() == 0){
             cout<<"The path is empty!"<<endl;
         }
         else if(route.size() == 1){
-            cout<<route[0]<<endl;
+            cout<<route[0].getId()<<endl;
         }
         else{
             unsigned int sz = route.size();
             unsigned int curr = 0;
-            cout<<route[0];
+            cout<<"BEGIN->"<<route[curr].getId();
             curr += 1;
             while(curr<sz){
-                cout<<"->"<<route[curr];
+                cout<<"->"<<route[curr].getId();
                 curr += 1;
             }
-            cout<<endl;
+            cout<<"->END"<<endl;
         }
     }
 };
@@ -76,19 +94,119 @@ public:
             }
         }
     }
-    // Path DFS(char from, char to){
-    //     Path ans;
-    //     if(maze.find(from) == maze.end()){
-    //         cout<<"Source vertix does not exist!"<<endl;
-    //         return ans;
-    //     }
-    //     if(maze.find(to) == maze.end()){
-    //         cout<<"Destination vertix does not exist!"<<endl;
-    //         return ans;
-    //     }
-    //     vector<Path> todo;
-    //     vector<Vertix> visited;
-    // }
+    vector<Vertix> findAdjacency(char id){
+        vector<Vertix> ans;
+        if(maze.find(id) == maze.end()){
+            cout<<"No such vertix in the graph!"<<endl;
+            return ans;
+        }
+        else{
+            map<char, int> vertixInfo = maze.find(id)->second;
+            vector<char> idList;
+            // Get a list of the name of all adjacent vertices
+            for(map<char, int>::iterator it = vertixInfo.begin(); it != vertixInfo.end(); it++){
+                idList.push_back(it->first);
+            }
+            // Construct a list of vertices based on the name list
+            for(unsigned int i=0; i<idList.size(); i++){
+                char currId = idList[i];
+                map<char, int> currInfo = maze.find(currId)->second; 
+                Vertix curr(currId, currInfo);
+                ans.push_back(curr);
+            }
+            return ans;
+        }
+    }
+    Path DFS(Vertix from, Vertix to){
+        Path ans;
+        if(maze.find(from.getId()) == maze.end()){
+            cout<<"Source vertix does not exist!"<<endl;
+            return ans;
+        }
+        if(maze.find(to.getId()) == maze.end()){
+            cout<<"Destination vertix does not exist!"<<endl;
+            return ans;
+        }
+        vector<Path> todo;
+        vector<Vertix> visited;
+        Path init;
+        init.addStop(from);
+        todo.push_back(init);
+        while(todo.size() != 0){
+            Path currentPath = todo.back();
+            todo.pop_back();
+            Vertix currentNode = currentPath.getLastNode();
+            if(currentNode == to){
+                return currentPath;
+            }
+            bool isVisited = false;
+            for(unsigned int i=0; i<visited.size(); i++){
+                if(visited[i] == currentNode){
+                    isVisited = true;
+                    break;
+                }
+                else{
+                    continue;
+                }
+            }
+            if(isVisited == false){
+                visited.push_back(currentNode);
+                vector<Vertix> currentAdjacent = findAdjacency(currentNode.getId());
+                for(unsigned int i=0; i<currentAdjacent.size(); i++){
+                    Path newPath = currentPath;
+                    newPath.addStop(currentAdjacent[i]);
+                    todo.push_back(newPath);
+                }
+            }
+        }
+        cout<<"No valid path exists!"<<endl;
+        return ans;   
+    }
+    Path BFS(Vertix from, Vertix to){
+        Path ans;
+        if(maze.find(from.getId()) == maze.end()){
+            cout<<"Source vertix does not exist!"<<endl;
+            return ans;
+        }
+        if(maze.find(to.getId()) == maze.end()){
+            cout<<"Destination vertix does not exist!"<<endl;
+            return ans;
+        }
+        queue<Path> todo;
+        vector<Vertix> visited;
+        Path init;
+        init.addStop(from);
+        todo.push(init);
+        while(todo.size() != 0){
+            Path currentPath = todo.front();
+            todo.pop();
+            Vertix currentNode = currentPath.getLastNode();
+            if(currentNode == to){
+                return currentPath;
+            }
+            bool isVisited = false;
+            for(unsigned int i=0; i<visited.size(); i++){
+                if(visited[i] == currentNode){
+                    isVisited = true;
+                    break;
+                }
+                else{
+                    continue;
+                }
+            }
+            if(isVisited == false){
+                visited.push_back(currentNode);
+                vector<Vertix> currentAdjacent = findAdjacency(currentNode.getId());
+                for(unsigned int i=0; i<currentAdjacent.size(); i++){
+                    Path newPath = currentPath;
+                    newPath.addStop(currentAdjacent[i]);
+                    todo.push(newPath);
+                }
+            }
+        }
+        cout<<"No valid path exists!"<<endl;
+        return ans;
+    }
 };
 
 
@@ -128,10 +246,9 @@ int main()
     maze.addVertix(G);
 
     // Test functionalities
-    Path way;
-    way.addStop(A);
-    way.addStop(B);
-    way.addStop(C);
-    way.printPath();
+    Path dfsAns = maze.DFS(A, C);
+    dfsAns.printPath();
+    Path bfsAns = maze.BFS(A, C);
+    bfsAns.printPath();
     return EXIT_SUCCESS;
 }
