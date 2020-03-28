6f5a7408ad3986de6cf6498d84eed44fba35c3bf
Xinhao Song <songxinhao@hotmail.com>
2020-03-28 00:44:46 -0500

    Update Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -47,15 +47,38 @@ void insertionSort(int * data, int n){
     }
 }
 
+// Helper function for the selection sort
+// Return the index of the smallest element in the unsorted region
+int findMin(int * data, int start, int n){
+    int min = start;
+    int curr = start + 1;
+    while(curr<n){
+        if(data[curr]<data[min]){
+            min = curr;
+        }
+        curr ++;
+    }
+    return min;
+}
+
+void selectionSort(int * data, int n){
+    for(int pos=0; pos<n; pos++){
+        int minIdx = findMin(data, pos, n);
+        if(minIdx != pos){
+            swap(data[pos], data[minIdx]);
+        }
+    }
+}
+
 int main()
 {
     int test1[9] = {45, 6, 13, 67, 76, 25, 18, 53, 32};
     int test2[24] = {16, 36, 70, 61, 45, 32, 51, 9, 22, 17, 46, 43, 83, 10, 38, 11, 47, 78, 57, 70, 43, 66, 61, 28};
     printArray(test1, 9);
-    insertionSort(test1, 9);
+    selectionSort(test1, 9);
     printArray(test1, 9);
     printArray(test2, 24);
-    insertionSort(test2, 24);
+    selectionSort(test2, 24);
     printArray(test2, 24);
     return EXIT_SUCCESS;
 }
